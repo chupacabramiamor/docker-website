6ba3417d99322a2dff1e084ba31f26c5b49f1b17
chupacabramiamor <chupacabramiamor@gmail.com>
2021-09-26 19:08:50 +0300

    Improvements

--- up.sh
+++ up.sh
@@ -11,10 +11,21 @@ PROJECT_NAME=$(grep PROJECT_NAME docker/.env | cut -d '=' -f 2-)
 CLIENT_NAME=$(grep CLIENT_NAME docker/.env | cut -d '=' -f 2-)
 DN="${PROJECT_NAME}.${CLIENT_NAME}"
 CONTAINER_NAME="${CLIENT_NAME}.${PROJECT_NAME}"
+NGINX_CONF="/etc/nginx/sites-available/$CLIENT_NAME"
+
+if [ ! -f $NGINX_CONF ]; then
+    sudo touch $NGINX_CONF
+    sudo ln -s "/etc/nginx/sites-available/${CLIENT_NAME}" /etc/nginx/sites-enabled/
+fi
+
+sed -e "s/\${DN}/${DN}/" -e "s/\${HTTP_PORT}/${HTTP_PORT}/" docker/nginx.conf.stub | sudo tee -a $NGINX_CONF
+sudo service nginx restart
 
 if [[ -z $(grep "${DN}" /etc/hosts) ]]; then
     echo "127.0.0.1 ${DN}" | sudo tee -a /etc/hosts
 fi
+
+sed -e "s/\${DN}/${DN}/" docker/apache-php/default.conf.stub | tee docker/apache-php/default.conf
 
 docker-compose --env-file=./docker/.env up -d
 
